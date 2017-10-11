d1727153a97ab12283aa3fde86d8c3115ef41ad1
lalit32024 <32644046+lalit32024@users.noreply.github.com>
2017-10-12 00:02:46 +0530

    Update news.py

--- news.py
+++ news.py
@@ -36,10 +36,9 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") == "news.search":
-        speech = "hi There" #"Invalid Action specified"
+    if req.get("result").get("action") != "news.search":
+        speech = "Invalid Action specified"
         return createResponse(speech, speech)
-
     yql_url = "https://newsapi.org/v1/articles?source=cnn&apiKey=6614fb3731b2472c9efa015800e01de3"
     result = urlopen(yql_url).read()
     data = json.loads(result)
