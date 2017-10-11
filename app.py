a7bfb06650bc6856d5ae64b4b7affdabce167692
lalit32024 <32644046+lalit32024@users.noreply.github.com>
2017-10-11 23:45:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def processRequest(req):
     if req.get("result").get("action") == "stockquote":
        from stock import processRequest
        res = processRequest(req)
-    if req.get("result").get("action") == "NEWS":
+    if req.get("result").get("action") == "news.search":
        speech = "hi there"
        return{
           "speech": speech,
