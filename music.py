10736783d80b87e286ff508e9bb7c987facc6aa3
lalit32024 <32644046+lalit32024@users.noreply.github.com>
2017-10-13 14:15:16 +0530

    Update music.py

--- music.py
+++ music.py
@@ -40,7 +40,7 @@ def processRequest(req):
     if req.get("result").get("action")!="searchmusic":
         speech = "Invalid Action specified"
         return createResponse(speech, speech)
-    yql_url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+req.Artist+"&api_key=c68de8a6159c02cd683804aa40debc53&format=json"
+    yql_url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+req.reuslt.parameters.Artist+"&api_key=c68de8a6159c02cd683804aa40debc53&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
     #return {
