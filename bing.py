9aa8a1109983dead20b6cff4ca88daac711ad1b0
lalit32024 <32644046+lalit32024@users.noreply.github.com>
2017-11-09 17:17:17 +0530

    Update bing.py

--- bing.py
+++ bing.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 # **********************************************
 # *** Update or verify the following values. ***
 # **********************************************
@@ -9,6 +9,7 @@
 
 from __future__ import print_function
 from future.standard_library import install_aliases
+
 install_aliases()
 
 from urllib.parse import urlparse, urlencode
@@ -33,21 +34,24 @@ subscriptionKey = "2f22ecfd9fa54027ba5976df791e98a7"
 host = "api.cognitive.microsoft.com"
 path = "/bing/v7.0/search"
 
-term = "iphoneX"
-def giveResult(request):
+term = "automata"
+
+
+def giveResult():
     if len(subscriptionKey) == 32:
         print('Searching the Web for: ', "iphoneX")
         headers, result = BingWebSearch(term)
         print("\nRelevant HTTP Headers:\n")
         print("\n".join(headers))
         print("\nJSON Response:\n")
-        data =json.dumps(json.loads(result), indent=4)
+        # print(json.dumps(json.loads(result), indent=4))
+        data = json.dumps(json.loads(result), indent=4)
         res = createResponse(data)
+        print(res)
         return res
     else:
         print("Invalid Bing Search API subscription key!")
         print("Please paste yours into the source code.")
-
 
 
 def BingWebSearch(search):
@@ -59,68 +63,70 @@ def BingWebSearch(search):
     print("\nSearch Url: ")
     print(conn.request)
     headers = [k + ": " + v for (k, v) in response.getheaders()
-                   if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
+               if k.startswith("BingAPIs-") or k.startswith("X-MSEdge-")]
     return headers, response.read().decode("utf8")
-   
+
+
 def createResponse(data):
-    val=data.get("webPages").get("value")
-    return {"speech":"see this on messenger",
-	    "displayText":"see this on messenger",
-	    "data": {
-             "facebook": {
-             "attachment": {
-	    "type":"template",
-            "payload":{
-             "template_type":"list",
-	     "top_element_style": "compact",
-            "elements":[
-            {
-             "title": val[0].get("name"),
-             "subtitle": val[0].get("snippet"),
-             "image_url":"",          
-	     "default_action": {
-               "type": "web_url",
-               "url": val[0].get("url")
-              }
-	    },
-	     {
-             "title": val[1].get("name"),
-             "subtitle": val[1].get("snippet"),
-             "image_url":"",          
-	     "default_action": {
-               "type": "web_url",
-               "url": val[1].get("url")
-              }
-	    },
-            {
-             "title": val[2].get("name"),
-             "subtitle": val[2].get("snippet"),
-             "image_url":"",          
-	     "default_action": {
-               "type": "web_url",
-               "url": val[2].get("url")
-              }
-	    },
-            {
-             "title": val[3].get("name"),
-             "subtitle": val[3].get("snippet"),
-             "image_url":"",          
-	     "default_action": {
-               "type": "web_url",
-               "url": val[3].get("url")
-              }
-	    }],
-      	"buttons": [
-          {
-            "title": "Read More",
-            "type": "postback",
-            "payload": "read more" 
-	  }
-        ] 
-      }}
-	}}
-}
-if __name__ == '__main__':
-   port = int(os.getenv('PORT', 5000))
-   print("Starting app on port %d" % port)
-   app.run(debug=True, port=port, host='0.0.0.0')
+    data=json.loads(data)
+    webPages=data.get("webPages")#.get("value")
+    val=webPages.get("value")
+    return {"speech": "see this on messenger",
+            "displayText": "see this on messenger",
+            "data": {
+                "facebook": {
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "list",
+                            "top_element_style": "compact",
+                            "elements": [
+                                {
+                                    "title": val[0].get("name"),
+                                    "subtitle": val[0].get("snippet"),
+                                    "image_url": "",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": val[0].get("url")
+                                    }
+                                },
+                                {
+                                    "title": val[1].get("name"),
+                                    "subtitle": val[1].get("snippet"),
+                                    "image_url": "",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": val[1].get("url")
+                                    }
+                                },
+                                {
+                                    "title": val[2].get("name"),
+                                    "subtitle": val[2].get("snippet"),
+                                    "image_url": "",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": val[2].get("url")
+                                    }
+                                },
+                                {
+                                    "title": val[3].get("name"),
+                                    "subtitle": val[3].get("snippet"),
+                                    "image_url": "",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": val[3].get("url")
+                                    }
+                                }],
+                            "buttons": [
+                                {
+                                    "title": "Read More",
+                                    "type": "postback",
+                                    "payload": "read more"
+                                }
+                            ]
+                        }}
+                }}
+            }
+
+
+giveResult()
